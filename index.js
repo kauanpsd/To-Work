b1bad7eaa9b5bb81912d2be9ddd7d3e1ca9a7257
Kauan Carvalho <kauan3210@gmail.com>
2023-06-11 14:50:03 -0300

    Remove Item function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, onValue, push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, onValue, push, remove} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL:"https://to-work-list-default-rtdb.firebaseio.com/"
@@ -15,7 +16,7 @@ const addItemBtn = document.getElementById("add-to-list");
 
 onValue(toDoListInDb,function(snapshot){
     clearHTML()
-    let doArray = Object.values(snapshot.val())
+    let doArray = Object.entries(snapshot.val())
     for( let task of doArray){
         addTaskToHTML(task)
     }
@@ -29,7 +30,21 @@ addItemBtn.addEventListener('click', function(){
 
 
 function addTaskToHTML(item){
-    taskItem.innerHTML += `<div class="task">${item}</div>` 
+
+    let itemId = item[0]
+    let itemName = item[1]
+
+    /* taskItem.innerHTML += `<div class="task">${itemName}</div>`  */
+    let newEl = document.createElement("li")
+    newEl.textContent = itemName
+
+    taskItem.append(newEl)
+
+    newEl.addEventListener("dblclick", function(){
+        let exactLocationTask = ref(database, `taskList/${itemId}`)
+        remove(exactLocationTask)
+    })
+
 }
 
 function clearHTML(){
